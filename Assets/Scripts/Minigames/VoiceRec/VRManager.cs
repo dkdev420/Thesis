88e23c9f48f1a4541dc61865d4a62e386f0004a8
Dario Ierimonti <darioierimonti@gmail.com>
2019-01-08 12:49:27 +0800

    ...

--- Assets/Scripts/Minigames/VoiceRec/VRManager.cs
+++ Assets/Scripts/Minigames/VoiceRec/VRManager.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class VRManager : MonoBehaviour
 {
+    public SimonBlock r, g, b;
+
 	void Start ()
 	{
-		
+        Thesis.HoloInputController.Instance.AddKeywords(new Dictionary<string, System.Action>
+        {
+            { "red", () => { r.PlaySound();  } },
+            { "blue", () => { g.PlaySound(); } },
+            { "green", () => { b.PlaySound(); } },
+        });
 	}
-	
-	void Update ()
-	{
-		
-	}
+
 }
